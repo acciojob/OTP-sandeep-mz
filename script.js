0f21cf863c268a104103828a74c7323a5935af69
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-05 16:51:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const codeInputs = document.querySelectorAll(".code");
+
+codeInputs.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+        const inputValue = e.target.value;
+
+        // Move to the next input when a digit is entered
+        if (inputValue.length === 1 && index < codeInputs.length - 1) {
+            codeInputs[index + 1].focus();
+        }
+
+        // Move to the previous input when backspace is pressed
+        if (inputValue.length === 0 && index > 0) {
+            codeInputs[index - 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+        // Prevent moving to the previous input when backspace is pressed
+        if (e.key === "Backspace" && index > 0) {
+            e.preventDefault();
+        }
+    });
+});
