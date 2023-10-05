f303a6c5e2015674c629c6018e23342f9144f23e
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-05 17:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//your JS code here. If required.
 const codeInputs = document.querySelectorAll(".code");
 
-codeInputs.forEach((input, index) => {
-    input.addEventListener("input", (e) => {
+// Function to handle input events
+function handleInput(index) {
+    return function (e) {
         const inputValue = e.target.value;
 
         // Move to the next input when a digit is entered
@@ -14,12 +14,25 @@ codeInputs.forEach((input, index) => {
         if (inputValue.length === 0 && index > 0) {
             codeInputs[index - 1].focus();
         }
-    });
+
+        // Check if all inputs are filled
+        if (Array.from(codeInputs).every((input) => input.value.length === 1)) {
+            alert("OTP verification successful!"); // You can customize this message
+        }
+    };
+}
+
+codeInputs.forEach((input, index) => {
+    input.addEventListener("input", handleInput(index));
 
     input.addEventListener("keydown", (e) => {
         // Prevent moving to the previous input when backspace is pressed
         if (e.key === "Backspace" && index > 0) {
             e.preventDefault();
         }
+    });
+
+    input.addEventListener("focus", (e) => {
+        e.target.select(); // Select the text when input is focused
     });
 });
